0a7eac4c72102c8355263a5af20de672caaebd5d
Noman <mdnoman70499@gmail.com>
2024-01-20 05:33:39 +0530

    two files are created

---
+++ 2nd_Day/Test/index.js
@@ -0,0 +1,11 @@
+// function x() {
+//     const a = 10; 
+// }
+// var xyz = 30;
+// x();// function exucution context is created 
+// var x = function () {
+//     console.log("I  am anonymouse function ");
+
+// }
+ ////     In ES6 
+// const fn = ()=>{}
