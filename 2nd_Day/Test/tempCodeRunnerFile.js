5aaba38887ba9a1932d5df3b916c5a6d172d5ffd
Noman <mdnoman70499@gmail.com>
2024-01-20 06:59:04 +0530

    added interviews prepation tips

---
+++ 2nd_Day/Test/tempCodeRunnerFile.js
@@ -0,0 +1,17 @@
+person.print();
+// person.print.call();
+// person.print.call(this);
+// person.print.call(person2);
+// person.print.call(person);
+
+// person.print();
+// person.print.call();
+// person.print.call(this);
+// person.print.call(person2);
+// person.print.call(person);
+
+// person.print();
+// person.print.call();
+// person.print.call(this);
+// person.print.call(person2);
+// person.print.call(person);
